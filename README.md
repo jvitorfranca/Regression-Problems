9df5b435eeefd81774f315e5a78805c49b2583a3
jvitorfranca <vitoorbr@gmail.com>
2018-11-14 11:21:56 -0300

    Preprocessing data with featuretools

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Regression-Problems
+
+Based on https://github.com/Featuretools/predict-remaining-useful-life
--- main.py
+++ main.py
@@ -3,4 +3,32 @@ import pandas as pd
 import utilities as ut
 
 
-data_path = 'NASA-Jet-Engine/train_FD004.txt'
+data_path = 'data/train_FD004.txt'
+
+dataset = ut.load_data(data_path)
+
+cutoff_times = ut.make_cutoff_times(dataset)
+
+es = ft.EntitySet('Dataset')
+
+es = es.entity_from_dataframe(entity_id='recordings',
+                              dataframe=dataset,
+                              index='index',
+                              time_index='time')
+
+es = es.normalize_entity(base_entity_id='recordings',
+                         new_entity_id='engines',
+                         index='engine_no')
+
+es = es.normalize_entity(base_entity_id='recordings',
+                         new_entity_id='cycles',
+                         index='time_in_cycles')
+
+features_matrix, features_defs = ft.dfs(entityset=es,
+                                        target_entity='engines',
+                                        agg_primitives=['sum', 'min', 'max', 'mode', 'mean'],
+                                        cutoff_time=cutoff_times,
+                                        verbose=True,
+                                        max_depth=3)
+
+targets = features_matrix.pop('RUL')
